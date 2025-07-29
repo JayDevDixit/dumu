e1308f1699e5a772402a579586703c302beb618a
Jay Dev Dixit <oberoy968@gmail.com>
2025-07-29 18:26:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let s = document.getElementById('switch');
+let btn = document.getElementById('btn')
+checked=false;
+// prop={
+//     true:{},
+//     false:{},
+// }
+btn.addEventListener('click',()=>{
+    if(checked){
+        btn.style.backgroundColor = 'var(--false)';
+        s.style.justifyContent = 'left'
+    }
+    else{
+        btn.style.backgroundColor = 'var(--true)';
+        s.style.justifyContent = 'right'
+    }
+  checked = !checked;  
+  console.log();
+})
